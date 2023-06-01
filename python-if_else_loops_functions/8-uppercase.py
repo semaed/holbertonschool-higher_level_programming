6ad16e932037a8efc0e694100864ebc7a4bfe692
semaed <figsaned@gmail.com>
2023-06-01 14:49:07 -0400

    Print strings uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            upper = chr(ord(char) - 32)
+        else:
+            upper = char
+        print("{}".format(upper), end="")
+    print()
