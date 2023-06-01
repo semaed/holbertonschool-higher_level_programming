13a291c00ccfd94b71b39f440cef7ba7fcef78bc
semaed <figsaned@gmail.com>
2023-06-01 14:07:46 -0400

    Print is lower or not

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    if c >= chr(97) and c <= chr(122):
+        return True
+    elif c >= chr(65) and c <= chr(90):
+        return False
+    else:
+        return
