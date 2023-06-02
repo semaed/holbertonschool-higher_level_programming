b2a2af02e453acaa97b80ab9443e9a224bf3860d
semaed <figsaned@gmail.com>
2023-06-02 15:29:13 -0400

    Script dynamic

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+args = sys.argv[1:]
+arg_count = len(args)
+if arg_count == 0:
+    print("0 arguments.")
+elif arg_count == 1:
+    print("1 argument:")
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
+else:
+    print("{} arguments: ".format(arg_count))
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
