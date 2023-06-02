8944ddc5095401dda39de63943e4822483a6f06c
semaed <figsaned@gmail.com>
2023-06-02 15:40:53 -0400

    Correction to the code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
+    import sys
 
-args = sys.argv[1:]
-arg_count = len(args)
-if arg_count == 0:
-    print("0 arguments.")
-elif arg_count == 1:
-    print("1 argument:")
-    for i, arg in enumerate(args, start=1):
-        print("{}: {}".format(i, arg))
-else:
-    print("{} arguments: ".format(arg_count))
-    for i, arg in enumerate(args, start=1):
-        print("{}: {}".format(i, arg))
+    args = sys.argv[1:]
+    arg_count = len(args)
+    if arg_count == 0:
+        print("0 arguments.")
+    elif arg_count == 1:
+        print("1 argument:")
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
+    else:
+        print("{} arguments: ".format(arg_count))
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
