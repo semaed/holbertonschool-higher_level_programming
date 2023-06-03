98f72a190c5f27955c06dac9703d962b4e24d341
semaed <figsaned@gmail.com>
2023-06-02 22:05:45 -0400

    Correction to the code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
+
+import sys
 if __name__ == "__main__":
-    import sys
 
     args = sys.argv[1:]
     arg_count = len(args)
     if arg_count == 0:
-File is present
-
-First line contains #!/usr/bin/python3
-
-Your program should run only as script, not as imported module
-
-Correct output - case: Hello
-
-Correct output - case: Hello There
-
-Correct output - case:
-
-Correct output - case: 98 Battery street
-        print("0 arguments.")
+        print("{} arguments.".format(arg_count))
     elif arg_count == 1:
-        print("1 argument:")
+        print("{} argument:".format(arg_count), end="\n")
         for i, arg in enumerate(args, start=1):
-            print("{}: {}".format(i, arg))
+            print("{}: {}".format(i, arg), end="\n")
     else:
         print("{} arguments: ".format(arg_count))
         for i, arg in enumerate(args, start=1):
-            print("{}: {}".format(i, arg))
+            print("{}: {}".format(i, arg), end="\n")
