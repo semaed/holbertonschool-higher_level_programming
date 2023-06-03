fab847f5f291b1affeab51c56e05cc4e0980eea4
semaed <figsaned@gmail.com>
2023-06-02 21:59:48 -0400

    Typo correction

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,11 +5,15 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
+
     adding = add(a, b)
     print("{} + {} = {}".format(a, b, adding), end="\n")
+
     subs = sub(a, b)
     print("{} - {} = {}".format(a, b, subs), end="\n")
+
     mult = mul(a, b)
     print("{} * {} = {}".format(a, b, mult), end="\n")
+
     quotient = div(a, b)
-    print("{} / {} = {}".foirmat(a, b, quotient), end="\n")
+    print("{} / {} = {}".format(a, b, quotient), end="\n")
