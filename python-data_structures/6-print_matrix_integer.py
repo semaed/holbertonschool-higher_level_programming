a2e41606bc7fca76c699b7709032a3bd8c90e29c
semaed <figsaned@gmail.com>
2023-06-06 15:02:25 -0400

    Correction to the code

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,11 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
     for row in matrix:
-        for i, element in enumerate(row):
-            if i < len(row) - 1:
-                print("{:d}".format(element), end=" ")
-            else:
-                print("{:d}".format(element))
-    print()
+        if row == []:
+            print("")
+        else:
+            for i, element in enumerate(row):
+                if i < len(row) - 1:
+                    print("{:d}".format(element), end=" ")
+                else:
+                    print("{:d}".format(element))
