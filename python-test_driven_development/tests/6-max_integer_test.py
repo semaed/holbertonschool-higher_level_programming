cac0be1c4d0852941b8231fca9bc821bc8c8ce98
semaed <figsaned@gmail.com>
2023-06-14 17:40:14 -0400

    Correction to the test file

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
 """
-This is the test file for the module max_integer.
+This module contains a unittest class for the function max_integer().
 """
 
 import unittest
-from max_integer import max_integer  # replace 'max_integer' with your module name
+from 6-max_integer import max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """
-    Defines a class for testing the function max_integer.
+    A unittest class for max_integer()
     """
-        def test_max_integer(self):
+    
+    def test_basic(self):
         """
-        Tests for the function max_integer.
+        Test normal cases for the function.
         """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-        self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([1]), 1)
-        
+        self.assertEqual(max_integer([4]), 4)
+        self.assertIsNone(max_integer([]))
+    def test_string(self):
+        """
+        Test if a TypeError is raised when the list contains strings.
+        """
         with self.assertRaises(TypeError):
-            max_integer([1, 2, 'three', 4])
+            max_integer(['a', 'b', 'c'])
+    def test_non_list(self):
+        """
+        Test if a TypeError is raised when the input is a string.
+        """
+        with self.assertRaises(TypeError):
+            max_integer("string")
+    def test_nested_list(self):
+        """
+        Test if a TypeError is raised when the list contains other lists.
+        """
+        with self.assertRaises(TypeError):
+            max_integer([[1, 2, 3], [4, 5, 6]])
 if __name__ == '__main__':
     unittest.main()
