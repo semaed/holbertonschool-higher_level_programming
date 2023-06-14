2a8df21f1781d59cd10a35e9257fda64bf63ecdf
semaed <figsaned@gmail.com>
2023-06-14 07:13:40 -0400

    Size validation-correction

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,12 +12,6 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a new instance of Square with 'size' as an attribute.
-
-        Args:
-            size: The size of the new square instance. Defaults to 0.
-                Raises:
-                    TypeError: If 'size' is not an integer.
-                    ValueError: If 'size' is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
