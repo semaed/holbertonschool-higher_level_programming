c8baaae6942a2065ee1f64ee2d452056aefced82
semaed <figsaned@gmail.com>
2023-06-16 14:16:34 -0400

    task 6 done with test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -20,28 +20,8 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([4]), 4)
-        self.assertIsNone(max_integer([]))
-
-    def test_string(self):
-        """
-        Test if a TypeError is raised when the list contains strings.
-        """
-        with self.assertRaises(TypeError):
-            max_integer(['a', 'b', 'c'])
-
-    def test_non_list(self):
-        """
-        Test if a TypeError is raised when the input is a string.
-        """
-        with self.assertRaises(TypeError):
-            max_integer("string")
-
-    def test_nested_list(self):
-        """
-        Test if a TypeError is raised when the list contains other lists.
-        """
-        with self.assertRaises(TypeError):
-            max_integer([[1, 2, 3], [4, 5, 6]])
+        self.assertEqual(max_integer([]), None)
+        self.assertRaises(TypeError, max_integer())
 
 
 if __name__ == '__main__':
