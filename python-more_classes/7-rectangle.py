eec8982d6b55240b676806cad9135664b5e639ff
semaed <figsaned@gmail.com>
2023-06-15 22:10:14 -0400

    Correction to the code

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -77,7 +77,9 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+        return "\n".join(
+            [str(self.print_symbol) * self.__width
+             for _ in range(self.__height)])
 
     def __repr__(self):
         """
