c22000e0746fb404914af148e575b340a5e35306
semaed <figsaned@gmail.com>
2023-06-20 09:01:49 -0400

    Corection to the code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Initialization method for Rectangle"""
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
