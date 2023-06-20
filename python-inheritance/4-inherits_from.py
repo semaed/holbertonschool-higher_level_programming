267597e6fa3fa0f346f5af222e6f305898b760be
semaed <figsaned@gmail.com>
2023-06-20 11:05:40 -0400

    Correction

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@ from the specified class."""
 
 
 def inherits_from(obj, a_class):
-    """ 
+    """
     Returns True if the object is an instance
     of a class that inherited (directly or indirectly)
     from the specified class, otherwise False.
