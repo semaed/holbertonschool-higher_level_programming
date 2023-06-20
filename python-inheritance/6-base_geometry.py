760685a9a1422a438c43abe619aba3b5ef547c5f
semaed <figsaned@gmail.com>
2023-06-20 07:58:13 -0400

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that contains a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class with public instance method area"""
+
+    def area(self):
+        """Method that raises an Exception with a message"""
+        raise Exception("area() is not implemented")
