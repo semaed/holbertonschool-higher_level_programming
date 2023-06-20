fe5a74ce371c892c01b01a2082bed4a21fe55503
semaed <figsaned@gmail.com>
2023-06-20 10:51:46 -0400

    Correction to the code

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -58,7 +58,7 @@ class Square(Rectangle):
         self.__size = size
 
     def area(self):
-        return self.__size * self.__size
+        return self.__size ** 2
 
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
