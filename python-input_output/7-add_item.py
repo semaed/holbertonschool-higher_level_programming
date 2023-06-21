26660988a42ab74859719137d07ba43441612a03
semaed <figsaned@gmail.com>
2023-06-21 14:22:39 -0400

    Write a script that adds all arguments to a Python list, and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a function that
+add all arguments to a Python list
+and save them to a file.
+"""
+import sys
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+try:
+    js_list = load_from_json_file("add_item.json")
+except Exception:
+    js_list = []
+
+for arg in sys.argv[1:]:
+    js_list.append(arg)
+save_to_json_file(js_list, "add_item.json")
