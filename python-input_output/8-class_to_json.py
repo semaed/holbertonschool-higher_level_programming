ae3bef405fb2e9e6f0733182a1d4d76cf56fbfeb
semaed <figsaned@gmail.com>
2023-06-21 15:16:52 -0400

    Correction to the code

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""
-This module contains a function that
-returns a dictionay description with simple
-data structure for JSOn serialization.
-"""
+''' Module n dictionary'''
 
 
-def get_json_description(obj):
-    """
-    Returns a dictionary
-     fo JSON serialization.
-    """
+def class_to_json(obj):
+    '''returns the dictionary description
+    with simple data structure '''
     return obj.__dict__
