83765492205a4b169ec5cf36d3a2506994e2ebdb
semaed <figsaned@gmail.com>
2023-06-21 09:55:57 -0400

    Correction to the code

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -8,6 +8,10 @@ an object (string).
 
 
 def to_json_string(my_obj):
+    """
+    Function that returns a JSON
+     representation of an object
+    """
     str = my_obj
     json_str = json.dumps(str)
     print(json_str)
