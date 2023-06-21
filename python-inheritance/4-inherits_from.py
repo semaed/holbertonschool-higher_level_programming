485be2a2ca7b0f48b1f726de798987f3a8a94584
semaed <figsaned@gmail.com>
2023-06-21 13:24:57 -0400

    Correction

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module that checks if an object is an instance
 of a class that inherited (directly or indirectly)
-from the specified class."""
+specified class."""
 
 
 def inherits_from(obj, a_class):
