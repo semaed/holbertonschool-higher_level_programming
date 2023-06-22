54d4f7c40c50a9e9ef6ee449aae3e63fc25885df
semaed <figsaned@gmail.com>
2023-06-22 14:59:32 -0400

    Correction

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Method that validates value"""
-        if not isinstance(name, str):
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
