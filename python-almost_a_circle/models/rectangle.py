e138b1a307238228933da799134989f19b7716fe
semaed <figsaned@gmail.com>
2023-06-25 20:44:26 -0400

    First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+"""
+This module contains the Base class.
+"""
+
+
+class Base:
+    """
+    Base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize Base instance
+
+        Args:
+            id (int, optional): id to assign to
+             the instance. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
+class Rectangle(Base):
+    """
+    Rectangle class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize Rectangle instance
+
+        Args:
+            width (int): width of the Rectangle
+            height (int): height of the Rectangle
+            x (int, optional): x of the Rectangle. Defaults to 0.
+            y (int, optional): y of the Rectangle. Defaults to 0.
+            id (int, optional): id of the Rectangle. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        Getter for width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for width
+
+        Args:
+            value (int): value to set width to
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter for height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter for height
+
+        Args:
+            value (int): value to set height to
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter for x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Setter for x
+
+        Args:
+            value (int): value to set x to
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Getter for y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Setter for y
+
+        Args:
+            value (int): value to set y to
+        """
+        self.__y = value
