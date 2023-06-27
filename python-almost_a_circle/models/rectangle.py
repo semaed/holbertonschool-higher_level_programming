390ceeda0a6cce420a95ab08470384d3e67a6dc5
semaed <figsaned@gmail.com>
2023-06-27 15:10:27 -0400

    Correction

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -171,4 +171,5 @@ class Rectangle(Base):
         """
         Method to return the dictionary representation of a Rectangle
         """
-        return {'id': self.id, 'width': self.__width, 'height': self.__height, 'x': self.__x, 'y': self.__y}
+        return {'id': self.id, 'width': self.__width, 'height': self.__height,
+                'x': self.__x, 'y': self.__y}
