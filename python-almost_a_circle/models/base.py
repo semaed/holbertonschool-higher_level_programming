d204a767fb656b10f596f226077c12d8b673016b
semaed <figsaned@gmail.com>
2023-06-27 08:43:31 -0400

    Correction

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -41,12 +41,15 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """
-        writes the JSON string representation of list_objs to a file
+        Writes the JSON string representation of list_objs
+        to a file
         """
-        filename = cls.__name__ + ".json"
-        list_dicts = []
-        if list_objs is not None:
-            for obj in list_objs:
-                list_dicts.append(cls.to_dictionary(obj))
-        with open(filename, 'w') as file:
-            file.write(cls.to_json_string(list_dicts))
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w') as fd:
+            list_instance = []
+            if list_objs is None:
+                fd.write(cls.to_json_string(list_instance))
+            else:
+                for i in list_objs:
+                    list_instance.append(i.to_dictionary())
+                fd.write(cls.to_json_string(list_instance))
