b78c7e082516d01de3401513b59e75d7457f4744
semaed <figsaned@gmail.com>
2023-06-27 14:29:56 -0400

    Correction to the code

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,25 +4,7 @@ This module contains the Base class.
 """
 
 
-class Base:
-    """
-    Base class
-    """
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """
-        Initialize Base instance
-
-        Args:
-            id (int, optional): id to assign to
-             the instance. Defaults to None.
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from models.base import Base
 
 
 class Rectangle(Base):
