85b5a8756aaf6304980e1e222abd28b9307dab34
semaed <figsaned@gmail.com>
2023-06-26 20:20:18 -0400

    Square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -203,10 +203,32 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        Getter for size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter for size
+
+        Args:
+            value (int): value to set size to
+        """
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Method to print the string representation of the Square
         """
+        id_str = str(self.id)
+        x_str = str(self.x)
+        y_str = str(self.y)
+        size_str = str(self.size)
         square_str = "[Square] (" + str(self.id) + ") " + str(self.x) + \
             "/" + str(self.y) + " - " + str(self.width)
         return square_str
