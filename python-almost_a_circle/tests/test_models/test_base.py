2fa2f321444478cbf095af96dc7336ef18477379
semaed <figsaned@gmail.com>
2023-06-29 14:34:52 -0400

    Correction

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,32 +1,59 @@
-#!/usr/bin/python3
-""" Module for test Base class """
-from models.base import Base
-from models.rectangle import Rectangle
 import unittest
-import json
+from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
 
 
-class TestBase(unittest.TestCase):
+class TestBaseMethods(unittest.TestCase):
+    """ Suite to test Base class """
 
-    # ... other tests ...
-
-    def test_to_json_string(self):
-        """Test to_json_string method"""
+    def setUp(self):
+        """ Method invoked for each test """
         Base._Base__nb_objects = 0
-        b1 = Base()
-        b1_dict = b1.to_dictionary()
-        json_dict = Base.to_json_string([b1_dict])
-        self.assertEqual(json_dict, json.dumps([b1_dict]))
-        self.assertEqual(Base.to_json_string(None), "[]")
-        self.assertEqual(Base.to_json_string([]), "[]")
 
-    def test_from_json_string(self):
-        """Test from_json_string method"""
-        Base._Base__nb_objects = 0
-        b1 = Base()
-        b1_dict = b1.to_dictionary()
-        json_dict = Base.to_json_string([b1_dict])
-        list_dicts = Base.from_json_string(json_dict)
-        self.assertEqual(list_dicts, [b1_dict])
-        self.assertEqual(Base.from_json_string(None), [])
-        self.assertEqual(Base.from_json_string("[]"), [])
+    def test_id(self):
+        """ Test assigned id """
+        new = Base(1)
+        self.assertEqual(new.id, 1)
+
+    def test_id_default(self):
+        """ Test default id """
+        new = Base()
+        self.assertEqual(new.id, 1)
+
+    def test_id_nb_objects(self):
+        """ Test nb object attribute """
+        new = Base()
+        new2 = Base()
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 2)
+        self.assertEqual(new3.id, 3)
+
+    def test_id_mix(self):
+        """ Test nb object attributes and assigned id """
+        new = Base()
+        new2 = Base(1024)
+        new3 = Base()
+        self.assertEqual(new.id, 1)
+        self.assertEqual(new2.id, 1024)
+        self.assertEqual(new3.id, 2)
+
+    def test_string_id(self):
+        """ Test string id """
+        new = Base('1')
+        self.assertEqual(new.id, '1')
+
+    def test_more_args_id(self):
+        """ Test passing more args to init method """
+        with self.assertRaises(TypeError):
+            new = Base(1, 1)
+
+    def test_access_private_attrs(self):
+        """ Test accessing to private attributes """
+        new = Base()
+        with self.assertRaises(AttributeError):
+            new.__nb_objects
