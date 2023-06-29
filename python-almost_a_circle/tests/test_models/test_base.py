bb1b93ef3f3e0b44713e535ef4577937837601a7
semaed <figsaned@gmail.com>
2023-06-29 15:11:41 -0400

    Correction

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,6 @@
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 import json
 
 
@@ -27,3 +28,22 @@ class TestBase(unittest.TestCase):
         self.assertEqual(list_dicts, [b1_dict])
         self.assertEqual(Base.from_json_string(None), [])
         self.assertEqual(Base.from_json_string("[]"), [])
+
+    def test_to_json_string(self):
+        """Test to_json_string method"""
+        r1 = Rectangle(1, 1)  # Create Rectangle instance
+        r1_dict = r1.to_dictionary()
+        json_dict = Rectangle.to_json_string([r1_dict])
+        self.assertEqual(json_dict, json.dumps([r1_dict]))
+        self.assertEqual(Rectangle.to_json_string(None), "[]")
+        self.assertEqual(Rectangle.to_json_string([]), "[]")
+
+    def test_from_json_string(self):
+        """Test from_json_string method"""
+        r1 = Rectangle(1, 1)  # Create Rectangle instance
+        r1_dict = r1.to_dictionary()
+        json_dict = Rectangle.to_json_string([r1_dict])
+        list_dicts = Rectangle.from_json_string(json_dict)
+        self.assertEqual(list_dicts, [r1_dict])
+        self.assertEqual(Rectangle.from_json_string(None), [])
+        self.assertEqual(Rectangle.from_json_string("[]"), [])
