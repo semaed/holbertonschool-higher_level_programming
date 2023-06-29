9f9c5ed4ce1bfd7e766efd59316d09072ff93f25
semaed <figsaned@gmail.com>
2023-06-29 13:39:00 -0400

    Correction

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,11 +2,6 @@
 """ Module for test Base class """
 import unittest
 from models.base import Base
-from models.square import Square
-from models.rectangle import Rectangle
-from io import StringIO
-from unittest import TestCase
-from unittest.mock import patch
 import json
 
 
