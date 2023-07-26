0eb9901653a3e6ae67871de990c728d02761fbee
semaed <figsaned@gmail.com>
2023-07-26 11:15:08 -0400

    Correction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
