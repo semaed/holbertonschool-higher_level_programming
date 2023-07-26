1e2d4d683004271ba5e065f249bf822afa6d6cbb
semaed <figsaned@gmail.com>
2023-07-26 12:00:59 -0400

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,6 @@
--- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Script that lists all shows contained in hbtn_0d_tvshows
+
+-- that have at least one genre linked.
 
 SELECT s.`title`, g.`genre_id`
 FROM `tv_shows` AS s
