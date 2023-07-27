100fc7b830b43c86ebc38d0ae8cf7ff33e18d5b8
semaed <figsaned@gmail.com>
2023-07-26 20:00:26 -0400

    Correction

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,9 +2,9 @@
 
 -- hbtn_0d_tvshows
 
-SELECT g.`name`
-FROM `tv_genres` AS g
-    INNER JOIN `tv_show_genres` AS s ON g.`id` = s.`genre_id`
-    INNER JOIN `tv_shows` AS t ON t.`id` = s.`show_id`
-WHERE t.`title` = "Dexter"
-ORDER BY g.`name`;
+SELECT tv_shows.title
+FROM tv_shows
+    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
