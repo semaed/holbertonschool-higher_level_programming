9ee802a16c442735d62d5b6e7edb3d123bf699ff
semaed <figsaned@gmail.com>
2023-07-27 13:28:22 -0400

    Correction

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,57 +1,19 @@
 #!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
 
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-
-def filter_states():
-    """
-    Connects to the MySQL server and lists all states with names starting with 'N'.
-
-    Arguments:
-        None (takes arguments from the command line)
-
-    Returns:
-        None
-    """
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".
-              format(sys.argv[0]))
-        return
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    try:
-        """" Connect to the MySQL server """
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=username,
-                             passwd=password,
-                             db=database_name)
-
-        """ Create a cursor object to execute queries """
-        cursor = db.cursor()
-
-        """ Execute the SELECT query """
-        cursor.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-        """ Fetch all the rows """
-        rows = cursor.fetchall()
-
-        """ Display the results """
-        for row in rows:
-            print(row)
-
-        """ Close the cursor and connection """
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-
-
-if __name__ == "__main__":
-    filter_states()
+    db_connection = MySQLdb.connect(host='localhost',
+                                    port=3306,
+                                    user=sys.argv[1],
+                                    passwd=sys.argv[2],
+                                    db=sys.argv[3])
+    cur = db_connection.cursor()
+    cur.execute("""SELECT * FROM states
+            WHERE name LIKE BINARY 'N%'
+            ORDER BY id ASC""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
