e6628eec581128f50f66fe2acbceedcb7b76b841
semaed <figsaned@gmail.com>
2023-07-27 12:56:04 -0400

    Correction

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-# Import necessary libraries
+""" Import necessary libraries """
 import sys
 import MySQLdb
 
-# Define the main function
+""" Define the main function """
 
 
 def list_states(username, password, database_name):
-    # Connect to the MySQL server
+    """ Connect to the MySQL server """
     db = MySQLdb.connect(
         host="localhost",
         user=username,
@@ -17,43 +17,43 @@ def list_states(username, password, database_name):
         port=3306
     )
 
-    # Create a cursor object to interact with the database
+    """ Create a cursor object to interact with the database """
     cursor = db.cursor()
 
     try:
-        # Execute the SQL query to retrieve all states from the table
-        #  in ascending order by id
+        """ Execute the SQL query to retrieve all states from the table """
+        """  in ascending order by id """
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-        # Fetch all rows from the result set
+        """ Fetch all rows from the result set """
         rows = cursor.fetchall()
 
-        # Display the results
+        """ Display the results """
         for row in rows:
             print(row)
 
     except Exception as e:
-        # If there is an error, print the error message
+        """ If there is an error, print the error message """
         print("Error:", e)
 
     finally:
-        # Close the cursor and database connection
+        """Close the cursor and database connection """
         cursor.close()
         db.close()
 
 
-# Check if the script is run as the main module
+""" Check if the script is run as the main module """
 if __name__ == "__main__":
-    # Check if all three arguments are provided (username, password,
-    #  database name)
+    """ Check if all three arguments are provided (username, password,
+    database name) """
     if len(sys.argv) != 4:
         print("Usage: python script_name.py <username> <password> <db_name>")
         sys.exit(1)
 
-    # Get the arguments from the command line
+    """ Get the arguments from the command line """
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Call the main function to list states from the database
+    """ Call the main function to list states from the database """
     list_states(username, password, database_name)
