db49cbe73828dcc7823a99e5ae537a919ad2dd71
semaed <figsaned@gmail.com>
2023-07-28 07:37:18 -0400

    Correction

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
 """
-return matching states; safe from MySQL injections
-# http://bobby-tables.com/python
-parameters given to script: username, password, database, state to match
+Return matching states; safe from MySQL injections
+
+Parameters given to script: username, password, database, state to match
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
 
-    # connect to database
+def get_matching_states(username, password, database, state_to_match):
+    """
+    Get matching states from the states table in the database.
+    """
+    # Connect to the database
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+                         user=username,
+                         passwd=password,
+                         db=database)
 
-    # create cursor to exec queries using SQL; match arg given
+    # Create cursor to execute queries using SQL; match arg given
     cursor = db.cursor()
     sql_cmd = """SELECT *
                  FROM states
                  WHERE name=%s ORDER BY id ASC"""
-    cursor.execute(sql_cmd, (argv[4],))
+    cursor.execute(sql_cmd, (state_to_match,))
 
+    # Fetch and print the matching rows
     for row in cursor.fetchall():
         print(row)
+
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    if len(argv) != 5:
+        print(
+            "Usage: {} <username> <password> <database> <state_name>".format(argv[0]))
+    else:
+        get_matching_states(argv[1], argv[2], argv[3], argv[4])
