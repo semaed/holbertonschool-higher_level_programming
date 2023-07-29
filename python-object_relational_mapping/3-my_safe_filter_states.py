1b750fd1ca4357bb4c1c4d93822e872797e94a6d
semaed <figsaned@gmail.com>
2023-07-29 13:31:13 -0400

    Correction

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,56 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N
+Script to display states from a MySQL database based on the given state_name.
 """
+
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    u_name = argv[1]
-    psw = argv[2]
-    base = argv[3]
-    search = argv[4]
 
-    # Connecting to MySQL database
-    db = MySQLdb.connect(host="localhost", user=u_name,
-                         passwd=psw, db=base, port=3306)
+def display_states(username, password, database, state_name):
+    """
+    Display states from the MySQL database.
 
-    # Creating cursor object
-    cur = db.cursor()
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Name of the MySQL database.
+        state_name (str): Name of the state to search for.
 
-    proc = ({search},)
-    # Executing MySql Query
-    query = "SELECT * FROM states WHERE name LIKE BINARY %s"
-    cur.execute(query, proc)
+    Returns:
+        None: The function prints the results directly to the console.
+    """
+    # Establish a connection to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+    cursor = db.cursor()
 
-    # Obtaining Query Result & prints the result in rows
-    rows = cur.fetchall()
-    for row in rows:
+    # Use a parameterized query to prevent MySQL injection
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    # Fetch and display the results
+    results = cursor.fetchall()
+    for row in results:
         print(row)
 
-    # Clean Up
-    cur.close()
+    # Close the cursor and connection
+    cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    """
+    Script entry point. Takes command-line arguments and calls display_states
+     function.
+    """
+    # Ensure the script is called with the correct number of arguments
+    if len(sys.argv) != 5:
+        print("Usage:", "./3-my_safe_filter_states.py", "<username>",
+              "<password>", "<database>", "<state_name>")
+
+        sys.exit(1)
+
+    # Get the arguments and call the display_states function
+    username, password, database, state_name = sys.argv[1:5]
+    display_states(username, password, database, state_name)
