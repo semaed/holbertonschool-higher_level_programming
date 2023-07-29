efa1a37e1ee83d33e885a04a4e0f9e51131958e6
semaed <figsaned@gmail.com>
2023-07-29 13:41:29 -0400

    Correction

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,56 +1,31 @@
 #!/usr/bin/python3
-"""
-Script to display states from a MySQL database based on the given state_name.
-"""
+'''
+takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+'''
 
 import MySQLdb
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_search = argv[4]
 
-def display_states(username, password, database, state_name):
-    """
-    Display states from the MySQL database.
+    connection = MySQLdb.connect(host='localhost', port=3306,
+                                 user=user, passwd=password,
+                                 db=db_name)
+    cursor = connection.cursor()
+    cursor.execute('''
+                   SELECT * FROM states
+                   WHERE name= %(state_search)s
+                   ORDER BY id
+                   ''', {'state_search': state_search})
 
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Name of the MySQL database.
-        state_name (str): Name of the state to search for.
-
-    Returns:
-        None: The function prints the results directly to the console.
-    """
-    # Establish a connection to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-    cursor = db.cursor()
-
-    # Use a parameterized query to prevent MySQL injection
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
-
-    # Fetch and display the results
-    results = cursor.fetchall()
-    for row in results:
+    # fetches all rows of a query result set and returns as tuple
+    q_rows = cursor.fetchall()
+    for row in q_rows:
         print(row)
-
-    # Close the cursor and connection
     cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    """
-    Script entry point. Takes command-line arguments and calls display_states
-     function.
-    """
-    # Ensure the script is called with the correct number of arguments
-    if len(sys.argv) != 5:
-        print("Usage:", "./3-my_safe_filter_states.py", "<username>",
-              "<password>", "<database>", "<state_name>")
-
-        sys.exit(1)
-
-    # Get the arguments and call the display_states function
-    username, password, database, state_name = sys.argv[1:5]
-    display_states(username, password, database, state_name)
+    connection.close()
