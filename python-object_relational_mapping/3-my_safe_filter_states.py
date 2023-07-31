80eb0f4235ef40844cd38e5923947da685057d58
semaed <figsaned@gmail.com>
2023-07-31 10:09:15 -0400

    Correction

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,23 +9,18 @@ from sys import argv
 
 if __name__ == '__main__':
     user = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    state_search = argv[4]
+    passwd = argv[2]
+    db = argv[3]
+    state_name = argv[4]
 
-    connection = MySQLdb.connect(host='localhost', port=3306,
-                                 user=user, passwd=password,
-                                 db=db_name)
-    cursor = connection.cursor()
-    cursor.execute('''
-                   SELECT * FROM states
-                   WHERE name= %(state_search)s
-                   ORDER BY id
-                   ''', {'state_search': state_search})
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, password=passwd, db=db)
 
-    # fetches all rows of a query result set and returns as tuple
-    q_rows = cursor.fetchall()
-    for row in q_rows:
-        print(row)
-    cursor.close()
-    connection.close()
+    cur = db.cursor()
+    protected = (state_name,)
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", protected)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
